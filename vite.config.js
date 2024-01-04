757ce0c7385a0508c962ff3bdbfa713e3e8f53c7
Vicktor007 <vicktord007@gmail.com>
2024-01-04 17:08:30 +0100

    save

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
+      "/api/": "https://eshop-api-t.onrender.com",
+      "/uploads/": "https://eshop-api-t.onrender.com",
     },
   },
 });
