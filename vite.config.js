d476345b158001492b2cc08b135368c74a6c355f
Vicktor007 <vicktord007@gmail.com>
2024-01-04 17:05:52 +0100

    save

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,32 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       "/api/": "http://localhost:5000",
-//       "/uploads/": "http://localhost:5000",
-//     },
-//   },
-// });
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: './src/main.jsx',
-    },
-  },
   server: {
     proxy: {
-      "/api/": "https://e-shop-api-bnt6.onrender.com", // the address that u serve in the backend 
+      "/api/": "http://localhost:5000",
+      "/uploads/": "http://localhost:5000",
     },
   },
-})
+});
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     manifest: true,
+//     rollupOptions: {
+//       input: './src/main.jsx',
+//     },
+//   },
+//   server: {
+//     proxy: {
+//       "/api/": "https://e-shop-api-bnt6.onrender.com", // the address that u serve in the backend 
+//     },
+//   },
+// })
